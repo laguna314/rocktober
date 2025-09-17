e803dac9159558723fe1fdac91843072b986b3bb
Jesus Anaya <31487994+laguna314@users.noreply.github.com>
2025-09-17 10:02:41 -0500

    Update App.jsx
    add spotify player

--- src/App.jsx
+++ src/App.jsx
@@ -123,6 +123,7 @@ export default function App() {
                 </div>
               ))}
             </div>
+            <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1zJQ2KauEHnFur6DCPGHEq?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         </div>
       </section>
